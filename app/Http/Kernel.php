719cc1e22c899c1a51e4632595351b44ea109410
minhnv <minh06081997@gmail.com>
2024-07-21 21:33:39 +0700

    log user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            LogUserActions::class,
         ],
 
         'api' => [
